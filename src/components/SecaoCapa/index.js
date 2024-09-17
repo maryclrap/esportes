50acb705651aadaef70d866dd57611c6ad4ecc1d
Clara <145566521+maryclrap@users.noreply.github.com>
2024-09-17 07:56:56 -0300

    Add files via upload

---
+++ src/components/SecaoCapa/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css';
+
+const Header = () => {
+    return(
+        <section className= 'secao-capa'>
+            <div className='capa center'>
+                <div className='texto-capa'>
+                    <p>Preços baixos em</p>
+                    <h1>Óculos de grau e de sol</h1>
+                    <p>Você só encontra aqui</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default SecaoCapa;
