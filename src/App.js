7ba56cc66ca813f53f3ee334177a4b1171971f8a
Clara <145566521+maryclrap@users.noreply.github.com>
2024-10-22 08:34:22 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Header from './componentes/Header';
 import SecaoCapa from './componentes/SecaoCapa';
+import Produtos from './componentes/Produtos';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <SecaoCapa/>     
+      <Produtos/>
     </div>
   );
 }
+
 
 export default App;
--- src/componentes/Produtos/index.js
+++ src/componentes/Produtos/index.js
@@ -4,63 +4,51 @@ import "./style.css";
 const Produtos = () => {
     return (
         <section id="produtos"> 
-        <div className="secao-produtos center">
-            <h2 className="titulos">NOSSOS PRODUTOS</h2>
-            <p>
-                Trabalhamos com bolas, tênis, 
-                roupas esportivas, visando seu conforto
-                e 
-            </p>
+            <div className="secao-produtos center">
+                <h2 className="titulos">NOSSOS PRODUTOS</h2>
+                <p>
+                    Trabalhamos com bolas, tênis, 
+                    roupas esportivas, visando seu conforto.
+                </p>
+                <p>
+                    Todos os nossos preços são acessíveis e contam com a melhor qualidade do mercado.
+                </p>
 
-            <p>
-                Todos os nossos preços são acessiveis e contam com a melhor qualidade do mercado
-            </p>
-
-            <div className="bolas-imgs">
-                <div className="card-bolas">
-                    <h3>Bola de Vôlei</h3>
-                    <picture>
-                        <img src= "./assets/volei.jpg" alt="Bola de vôlei"></img>
-                    </picture>
-                    <p>R$ 699,99</p>
-                </div>
-                <div className="card-bolas">
-                    <h3>Bola de Basquete</h3>
-                    <picture>
-                        <img src= "./assets/" alt="Bola de vôlei"></img>
-                    </picture>
-                    <p>R$ 249,90</p>
-                </div>
-                <div className="card-bolas">
-                    <h3>Bola de Futebol</h3>
-                    <picture>
-                        <img src= "./assets/volei.jpg" alt="Bola de vôlei"></img>
-                    </picture>
-                    <p>R$ 499,99</p>
-                </div>
-                <div className="card-bolas">
-                    <h3>Bola de Handebol</h3>
-                    <picture>
-                        <img src= "./assets/volei.jpg" alt="Bola de vôlei"></img>
-                    </picture>
-                    <p>R$ 149,90</p>
+                <div className="bolas-imgs">
+                    <div className="card-bolas">
+                        <h3>Bola de Vôlei</h3>
+                        <img src='./assets/volei.jpg' alt="Bola de vôlei" />
+                        <p>R$ 699,99</p>
+                    </div>
+                    <div className="card-bolas">
+                        <h3>Bola de Basquete</h3>
+                        <img src="./assets/basquete.jfif" alt="Bola de basquete" />
+                        <p>R$ 249,90</p>
+                    </div>
+                    <div className="card-bolas">
+                        <h3>Bola de Futebol</h3>
+                        <img src="./assets/fut.jpg" alt="Bola de futebol" />
+                        <p>R$ 499,99</p>
+                    </div>
+                    <div className="card-bolas">
+                        <h3>Bola de Handebol</h3>
+                        <img src="./assets/hand.jpg" alt="Bola de handebol" />
+                        <p>R$ 149,90</p>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div className="lista-produtos-center">
-            <p>Todos os nossos produtos incluem:</p>
-            <ul>
-            <li>Garantia de 1 ano</li>
-            <li>Manutenção preventiva</li>
-            <li>Descontos especiais na compra da segunda unidade</li>
-            <li>Flexibilidade de pagamentos</li>
-            </ul>
-            
+            <div className="lista-produtos center">
+                <p>Todos os nossos produtos incluem:</p>
+                <ul>
+                    <li>Garantia de 1 ano</li>
+                    <li>Manutenção preventiva</li>
+                    <li>Descontos especiais na compra da segunda unidade</li>
+                    <li>Flexibilidade de pagamentos</li>
+                </ul>
             </div>
-        
         </section>
-    )
+    );
 }
 
 export default Produtos;
